217aa7b2ce446bd59ab51f458301179ff023488f
hrushiph <hrushi.ph@gmail.com>
2021-05-04 19:46:00 -0700

    Checking deployment

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/home')
+@app.route('/home')
 def home():
     return "<h5>Data Analytics From Group Wizards at Work...</h5>"
